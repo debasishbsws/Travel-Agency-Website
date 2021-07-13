031ba938ca74fd175ffe40655844d87b2d2a3509
Debasish Biswas <65381620+Debasishbsws@users.noreply.github.com>
2021-07-13 20:18:47 +0530

    Authenticate with Google

---
+++ public/app.js
@@ -0,0 +1,22 @@
+
+const auth = firebase.auth();
+
+const signInBtn = document.getElementById('signInBtn');
+const signOutBtn = document.getElementById('signOutBtn');
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+
+auth.onAuthStateChanged(user =>{
+    if(user){
+        //signed in
+        signInBtn.hidden = true;
+        signOutBtn.hidden = false;
+
+    } else {
+        // not signed in
+        signInBtn.hidden = false;
+        signOutBtn.hidden = true;
+    }
+});
